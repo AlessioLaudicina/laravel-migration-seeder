20d3a27a7bc29f65efd8b213eb3fd869aa23b4a7
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-05-12 15:52:23 +0200

    first commit

---
+++ database/migrations/2023_05_12_132934_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_partenza', 30);
+            $table->string('stazione_arrivo', 30);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('codice_treno');
+            $table->boolean('in_orario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,1 @@
+<h1 class="text-center py-4">I nostri treni</h1>
